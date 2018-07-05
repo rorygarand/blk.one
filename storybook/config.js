6b695ebc9399287448954cf3276f0837e2c91b7f
Rory Garand <github@rorygarand.com>
2018-07-05 14:03:33 -0500

    configure storybook

---
+++ storybook/config.js
@@ -0,0 +1,8 @@
+import { configure } from '@storybook/react';
+
+function loadStories() {
+  require('../src/components/stories.js');
+  require('../src/views/stories.js');
+}
+
+configure(loadStories, module);
