cc3d097d7d380a159b1923e19bdb1438adf500aa
Rory Garand <github@rorygarand.com>
2018-07-05 18:04:45 -0500

    initial pass

--- src/views/main/index.js
+++ src/views/main/index.js
@@ -1,13 +1,96 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+import { Button, Header } from '../../components';
+
+import banner from '../../images/banner.jpg';
+import { styles } from '../../utils';
 
 class Main extends Component {
   render() {
     return (
-      <div className="Main">
-        starting point
-      </div>
+      <View id="main">
+        <Header name="EOS User" />
+        <Content>
+          <Wrapper>
+            <Row>
+              <span>most recent blocks</span>
+              <Button title="load" />
+            </Row>
+          </Wrapper>
+        </Content>
+      </View>
     );
   }
 }
 
 export default Main;
+
+const Content = styled.section`
+  align-items: center;
+  bottom: 0;
+  display: flex;
+  position: absolute;
+  overflow: auto;
+  justify-content: center;
+  top: ${styles.header.outerHeight - 8}px;
+  width: 100%;
+
+  &::-webkit-scrollbar {
+    margin-right: 5px;
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: rgba(144, 164, 174, 0.1);
+    border-radius: 10px;
+    margin-top: 1px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(144, 164, 174, 0.3);
+    border-radius: 10px;
+    margin: 4px;
+    -webkit-transition: background-color 200ms ease-in-out;
+  }
+
+  &:hover::-webkit-scrollbar-thumb {
+    background-color: rgba(144, 164, 174, 0.5);
+    border-radius: 10px;
+    margin: 4px;
+  }
+`;
+
+const Row = styled.div`
+  border-bottom: 1px solid ${styles.almostWhite};
+  display: flex;
+  flex-direction: row;
+  height: ${styles.row.height}px;
+  justify-content: space-between;
+  line-height: ${styles.row.height}px;
+  padding: 0 8px;
+  text-transform: uppercase;
+`;
+
+const View = styled.div`
+  background-image: url(${banner});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 50% center;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  font-family: Quicksand;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+`;
+
+const Wrapper = styled.div`
+  background-color: white;
+  border-radius: 2px;
+  box-shadow: ${styles.lightGrey} 0px 0px 1px 0px;
+  flex: 0.7;
+  min-height: 80%;
+`;
--- src/views/main/index.test.js
+++ src/views/main/index.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import Main from './index';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Main />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Main', () => {
+  it('renders without crashing', () => {
+    const component = shallow(<Main />);
+    expect(component.find('#main').exists()).toBe(true);
+  });
 });
