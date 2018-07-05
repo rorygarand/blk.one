a2b5596d3292daa6dd06d94f130a8ee0c2f8b5ab
Rory Garand <github@rorygarand.com>
2018-07-05 14:20:03 -0500

    remove unused import

--- src/components/header/story.js
+++ src/components/header/story.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import Header from './index';
 
 storiesOf('Header', module)
