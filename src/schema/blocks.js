e10fccf7c4f2e40377b6dfcfd12b9af327de0078
Rory Garand <github@rorygarand.com>
2018-07-06 12:12:05 -0500

    separate block and blocks

--- src/schema/blocks.js
+++ src/schema/blocks.js
@@ -1,20 +1,20 @@
 import PropTypes from 'prop-types';
 
-export const blocks = PropTypes.arrayOf(
-  PropTypes.shape({
-    action_mroot: PropTypes.string.isRequired,
-    block_num: PropTypes.number.isRequired,
-    confirmed: PropTypes.number.isRequired,
-    id: PropTypes.string.isRequired,
-    new_producers: PropTypes.string,
-    previous: PropTypes.string.isRequired,
-    producer: PropTypes.string.isRequired,
-    producer_signature: PropTypes.string.isRequired,
-    ref_block_prefix: PropTypes.number.isRequired,
-    schedule_version: PropTypes.number.isRequired,
-    timestamp: PropTypes.string.isRequired,
-    transaction_mroot: PropTypes.string.isRequired
-  })
-);
+export const block = PropTypes.shape({
+  action_mroot: PropTypes.string.isRequired,
+  block_num: PropTypes.number.isRequired,
+  confirmed: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
+  new_producers: PropTypes.string,
+  previous: PropTypes.string.isRequired,
+  producer: PropTypes.string.isRequired,
+  producer_signature: PropTypes.string.isRequired,
+  ref_block_prefix: PropTypes.number.isRequired,
+  schedule_version: PropTypes.number.isRequired,
+  timestamp: PropTypes.string.isRequired,
+  transaction_mroot: PropTypes.string.isRequired
+});
+
+export const blocks = PropTypes.arrayOf(block);
 
 export default { blocks };
