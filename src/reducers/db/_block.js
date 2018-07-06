a1171b7614bc67f02af8265848298df763c77600
Rory Garand <github@rorygarand.com>
2018-07-05 22:43:49 -0500

    typo fix

--- src/reducers/db/_block.js
+++ src/reducers/db/_block.js
@@ -13,18 +13,20 @@ const initialState = {
 
 const block = (state = initialState, action) => {
   switch (action.type) {
-    case GET_BLOCKS_ERROR:
+    case GET_BLOCKS_ERROR: {
       const nextState = dotProp.merge(state, 'isError', true);
       return {
         nextState,
         isLoading: false
       };
-    case GET_BLOCKS_LOADING:
+    }
+    case GET_BLOCKS_LOADING: {
       const nextState = dotProp.merge(state, 'isLoading', true);
       return {
         nextState,
         isError: false
       };
+    }
     case GET_BLOCKS_SUCCESS: {
       const nextState = dotProp.merge(state, 'data', action.payload.data);
       return {
