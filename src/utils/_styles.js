3be3f89a13d00ffb30438b22234a52434827fc3e
Rory Garand <github@rorygarand.com>
2018-07-06 11:15:58 -0500

    add clickable variant

--- src/utils/_styles.js
+++ src/utils/_styles.js
@@ -4,6 +4,7 @@ import variables from './_variables';
 
 const Row = styled.div`
   border-bottom: 1px solid ${variables.almostWhite};
+  cursor: ${({ clickable }) => (clickable ? 'pointer' : 'initial')};
   display: flex;
   flex-direction: row;
   height: ${({ height }) => height || 30}px;
